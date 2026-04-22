419a063aa6227f99d0af33e97e4ac3606fe70108
U-Madhu <U-Madhu@users.noreply.github.com>
2026-04-23 01:50:41 +0530

    homepage updating

--- server/server.js
+++ server/server.js
@@ -255,7 +255,37 @@ server.post('/google-auth', async (req, res) => {
     });
 });
 
-server.post('/create-blog',verifyJWT,(req,res)=>{
+
+server.get('/latest-blogs',(req,res)=>{
+    let maxLimit=5;
+    Blog.find({draft:false})
+    .populate("author","personal_info.profile_image personal_info.username personal_info.fullname -_id")
+    .sort({'publishedAt':-1})
+    .select('blog_id title des banner activity tags publishedAt -_id')
+    .limit(maxLimit)
+    .then(blogs=>{
+        return res.status(200).json({blogs})
+    })
+    .catch(err=>{
+        return res.status(500).json({error:err.message})
+    })
+})
+
+server.get("/trending-blogs",(req,res)=>{
+    Blog.find({draft:false})
+    .populate("author","personal_info.profile_image personal_info.username personal_info.fullname -_id")
+    .sort({'activity.total_read':-1,"activity.total_likes":-1,"publishedAt":-1})
+    .select("blog_id title publishedAt -_id")
+    .limit(5)
+    .then(blogs=>{
+        return res.status(200).json({blogs})
+    })
+    .catch(err=>{
+        return res.status(500).json({error:err.message})
+    })
+})
+
+server.post('/create-blogs',verifyJWT,(req,res)=>{
     
     let authorId=req.user;
 
