f4c40ccb1978c202e6e86903ef5624da39d84240
U-Madhu <U-Madhu@users.noreply.github.com>
2026-06-14 17:45:24 +0530

    like and comments page updates

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ import jwt from "jsonwebtoken";
 import cors from 'cors';
 import { getAuth } from 'firebase-admin/auth';
 import serviceAccountKey from './campus-space-01-firebase-adminsdk-fbsvc-864f73cdf1.json' with { type: "json" };
+import Notification from './Schema/Notification.js'
+
+import Commeng from './Schema/Comment.js'
+
 // importing schemas
 import User from "./Schema/User.js";
 import Blog from './Schema/Blog.js'
@@ -16,6 +20,7 @@ import firebaseAdmin from "firebase-admin";
 
 //aws
 import aws from "aws-sdk";
+import CommentField from "../frontend/src/components/comment-field.component.jsx";
 
 
 //regex 
@@ -477,6 +482,123 @@ server.post("/get-blog",(req,res)=>{
     })
        
 })
+
+server.post("/like-blog",verifyJWT,(req,res)=>{
+    let user_id=req.user;
+
+    let {_id,islikedByUser}=req.body;
+
+    let incrementVal=!islikedByUser?1:-1; 
+
+    Blog.findOneAndUpdate({_id},{$inc:{"activity.total_likes":incrementVal} })
+    .then(blog=>{
+        
+        if(!islikedByUser){
+            let like=new Notification({
+                type:"like",
+                blog:_id,
+                notification_for:blog.author,
+                user:user_id
+
+
+            })
+            like.save().then(notification=>{
+                return res.status(200).json({liked_by_user:true})
+            })
+
+        }else{
+            Notification.findOneAndDelete({usee:user_id,blog:_id,type:"like"})
+            .then(data=>{
+                return res.status(200).json({liked_by_user:false})
+            })
+            .catch(err=>{
+                return res.status(500).json({error:err.message})
+            })
+        }
+         
+    })
+})
+
+server.post("/isliked-by-user",verifyJWT,(req,res)=>{
+    let user_id=req.user;
+    let{_id}=req.body;
+    Notification.exists({user:user_id,type:"Like",blog:_id})
+    .then(result=>{
+        return res.status(200).json({result})
+    })
+    .catch(err=>{
+        return res.status(500).json({error:err.message})
+    })
+
+
+})
+
+server.post("/add-comment",verifyJWT,(req,res)=>{
+
+    let user_id=req.user;
+
+    let{_id,comment,replying_to,blog_author}=req.body;
+
+    if(!comment.length){
+        return res.status(403).json({error:'Write something to leave a comment'});
+
+    }
+
+    // creating a comment doc 
+
+    let commentObj=new Comment({
+        blog_id:_id,
+        blog_author,
+        comment,
+        commented_by:user_id,
+    })
+    commentObj.save().then(commentFile=>{
+        let {comment,commentedAt,children}=commentFile;
+
+        Blog.findOneAndUpdate({_id},{$push:{"comments":commentFile._id},$inc:{"activity.total_comments":1},"activity.total_parent_comments":1})
+        .then(blog=>{
+            console.log('New comment created')
+        })
+        
+        let notificationObj={
+            type:"comment",
+            blog:_id,
+            notification_for:blog_author,
+            user:user_id,
+            comment:commentFile._id
+        }
+        new Notification(notificationObj).save().then(notification=>console.log('new notificaaation created'));
+
+        return res.status(200).json({
+            comment,commentedAt,_id:commentFile._id,user_id,children
+        })
+    })
+})
+
+server.post("/get-blog-comments",(req,res)=>{
+
+    let {blog_id,skip}=req.body;
+
+    let maxLimit=5;
+
+    Comment.find({blog_id,isReply:false})
+    .populate("commented_by","personal_info.username personal_info.fullname personal_info.profile_img")
+    .skip(skip)
+    .limit(maxLimit)
+    .sort({
+        'commentedAt':-1
+    })
+    .then(comment=>{
+        return res.status(200).json(comment)
+    })
+    .catch(err=>{
+        //console.log(err,message);
+        return res.status(500).json({error:err.message})
+    })
+
+
+})
+
 server.listen(PORT,()=>{
     console.log(`listening on port : http://localhost:${ PORT}`);
 })
