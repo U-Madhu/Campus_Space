162e8b143971068d1d1ed25d904b1c078ca620d9
U-Madhu <U-Madhu@users.noreply.github.com>
2026-06-15 18:57:13 +0530

    profile edit page update

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ import firebaseAdmin from "firebase-admin";
 import aws from "aws-sdk";
 import CommentField from "../frontend/src/components/comment-field.component.jsx";
 import { populate } from "dotenv";
+import { verify } from "jsonwebtoken";
 
 
 //regex 
@@ -441,6 +442,75 @@ server.post('/get-profile',async (req,res)=>{
         res.status(500).json({error:err.message})
     })
     
+})
+
+server.post('/update-profile-img',verifyJWT,(req,res)=>{
+
+    let {url}=req.body;
+
+    User.findOneAndUpdate({_id:req.user},{"personal_info.profile_img":url})
+    .then(()=>{
+        return res.status(200).json({profile_image:url})
+    })
+    .catch(err=>{
+        return res.status(500).json({error:err.message})
+    })
+})
+
+
+server.post('/update-profile',verifyJWT,(req,res)=>{
+    let {username,bio,social_links}=req.body;
+
+    let bioLimit=150;
+
+    if(username.length<3){
+        return res.status(403).json({error:"Username should be at least 3 letter long"})
+
+    }
+    if(bio.length>bioLimit){
+        return res.status(403).json({error:`Bio should not be morethan ${bioLimit} characters`})
+    }
+
+    let socialLinksArr=Object.keys(social_links);
+
+    try{
+
+        for(let i=0;i<socialLinksArr.length;i++){
+            if(social_links[socialLinksArr[i]].length){
+                let hostname =new URL(social_links[socialLinksArr[i]]).hostname;
+
+                if(!hostname.includes(`${socialLinksArr[i]}.com`)&& socialLinksArr[i]!='website'){
+                    return res.status(403).json({error:`${socialLinksArr[i]} link is invalid. you must enter a full link`})
+                }
+
+            }
+        }
+
+    }
+    catch(err){
+        return res.status(500).json({error:"You must provide full social links with http(s) included"})
+    }
+
+    let updateObj={
+        "personal_info.username":username,
+        "personal_info.bio":bio,
+        social_links
+    }
+
+    User.findOneAndUpdate({_id:req.user},updateObj,{
+        runValidators:true
+    })
+    .then(()=>{
+        return res.status(200).json({username})
+    })
+    .catch(err=>{
+        if(err.code==11000){
+            return res.status(409).json({error:"username is already taken"})
+        }
+        return res.status(500).json({error:err.message})
+
+    })
+
 })
 
 server.post('/create-blogs',verifyJWT,(req,res)=>{
